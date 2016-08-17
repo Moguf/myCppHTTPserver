b11e569b1617a8aff6199ed2c519ffa0788d44ec
Moguf <kbu94985@gmail.com>
2016-08-18 00:00:43 +0900

    MOD:Cpp_Easy_UDP_Server.cpp

--- src/Cpp_Easy_UDP_Server.cpp
+++ src/Cpp_Easy_UDP_Server.cpp
@@ -1,4 +1,25 @@
 #include <iostream>
+#include <boost/asio.hpp>
 
-using boost::asio;
+using namespace boost::asio;
+
+char buff[512];
 io_service service;
+boost::asio::ip::udp::endpoint sender_ep;
+ip::udp::socket sock(service);
+
+void on_read(const boost::system::error_code & err,std::size_t read_bytes){
+    std::cout << "read " << read_bytes << std::endl;
+    sock.async_receive_from(buffer(buff), sender_ep,on_read);
+}
+
+int main(void){
+    ip::udp::endpoint ep(ip::address::from_string("127.0.0.1"),8001);
+    sock.open(ep.protocol());
+    sock.set_option(boost::asio::ip::udp::socket::reuse_address(true));
+    sock.bind(ep);
+    sock.async_receive_from(buffer(buff,512),sender_ep,on_read);
+    service.run();
+
+    return 0;
+}
--- src/README.md
+++ src/README.md
@@ -1,4 +1,25 @@
-##
+# CppHTTPserver
+Fundamental Server with C++.
+
+## Requirements.
+- boost > 1.58
+
+## Build
+
+## Run
+
+## Boost.Asio namespaces
+
+## IP addresses
+
+## Endpoints
+
+
+## Sockets
+
+## Socket member functions
+
+## Connecting-related functions
 
 
 ## References
